f9885e872e360f46b2aa70e0331b0718b5cdeb89
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-24 20:12:34 +0300

    feat: make a simple page with Genesis 1

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,22 @@
-export default function IndexPage() {
-  return "Jesus is LORD";
+export interface Verse {
+  number: number;
+  verse: string;
+}
+
+export const apiUrl = "https://open-bible-api.vercel.app";
+
+export default async function IndexPage() {
+  const response = await fetch(`${apiUrl}/GEN/1`);
+  const data: Verse[] = await response.json();
+
+  return (
+    <div className="p-3">
+      {data.map((verse, index) => (
+        <p key={index}>
+          <small>{verse.number} </small>
+          <span>{verse.verse}</span>
+        </p>
+      ))}
+    </div>
+  );
 }
