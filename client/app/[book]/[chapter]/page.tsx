a6337f2a5235c559e4f1b6964c1990e6e10f9282
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-24 20:39:09 +0300

    chore: leave it at that for now

--- client/app/[book]/[chapter]/page.tsx
+++ client/app/[book]/[chapter]/page.tsx
@@ -1,5 +1,3 @@
-import { redirect } from "next/navigation";
-
 export interface Verse {
   number: number;
   verse: string;
@@ -20,42 +18,14 @@ export default async function IndexPage({ params }: Props) {
   const response = await fetch(`${apiUrl}/${book}/${chapterNumber}`);
   const data: Verse[] = await response.json();
 
-  const prevPage = () => {
-    let newChapter = chapterNumber - 1;
-    let newBook = book;
-    redirect(`/${newBook}/${newChapter}`);
-  };
-
-  const nextPage = () => {
-    let newChapter = chapterNumber + 1;
-    let newBook = book;
-    redirect(`/${newBook}/${newChapter}`);
-  };
-
   return (
-    <>
-      <div className="p-3">
-        {data.map((verse, index) => (
-          <p key={index}>
-            <small>{verse.number} </small>
-            <span>{verse.verse}</span>
-          </p>
-        ))}
-      </div>
-      <div className="px-3 flex justify-between items-center">
-        <button
-          className="bg-stone-600 text-white rounded-md p-1"
-          onClick={prevPage}
-        >
-          Prev
-        </button>
-        <button
-          className="bg-stone-600 text-white rounded-md p-1"
-          onClick={nextPage}
-        >
-          Next
-        </button>
-      </div>
-    </>
+    <div className="p-3">
+      {data.map((verse, index) => (
+        <p key={index}>
+          <small>{verse.number} </small>
+          <span>{verse.verse}</span>
+        </p>
+      ))}
+    </div>
   );
 }
