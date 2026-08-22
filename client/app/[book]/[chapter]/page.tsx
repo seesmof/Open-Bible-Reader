da88ee56d1a4dcf2653da322a12a18525a6ed86b
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-22 11:53:51 +0300

    refactor: change the modal key to S

--- client/app/[book]/[chapter]/page.tsx
+++ client/app/[book]/[chapter]/page.tsx
@@ -76,7 +76,7 @@ export default function ChapterPage({
   const handleKeyDown = (e: KeyboardEvent) => {
     if (e.key == "ArrowLeft") navgiateToPreviousChapter();
     else if (e.key == "ArrowRight") navgiateToNextChapter();
-    else if (e.key == "c") setIsModalOpen((isModalOpen) => !isModalOpen);
+    else if (e.key == "s") setIsModalOpen((isModalOpen) => !isModalOpen);
     else if (e.key == "Escape") {
       setIsModalOpen(false);
       setSelectedBook(null);
